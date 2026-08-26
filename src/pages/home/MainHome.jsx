e1720aa4ce662756de71c69b8afabccfde329bcb
daniel689671 <nguyennguyen7510159@gmail.com>
2026-08-26 12:09:46 +0100

    don

--- src/pages/home/MainHome.jsx
+++ src/pages/home/MainHome.jsx
@@ -343,7 +343,7 @@ export default function MainHome() {
 
           <div className="flex items-center gap-6 pt-4">
             <a
-              href="https://credixa-virid.vercel.app"
+              href="https://credixa.co/"
               target="_blank"
               rel="noopener noreferrer"
               className="text-lime-300 flex underline items-center gap-2 hover:text-lime-600"
